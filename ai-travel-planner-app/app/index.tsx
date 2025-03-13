7a63e2909ed8aec5d570b330d67f0497ca37b8e8
TuanLQHe171554 <tuanlqhe171554@fpt.edu.vn>
2025-03-13 22:48:29 +0700

    Landing Screen UI

--- ai-travel-planner-app/app/index.tsx
+++ ai-travel-planner-app/app/index.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native";
-
+import Login from "./../component/Login";
 export default function Index() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Login/>
     </View>
   );
 }
---
+++ ai-travel-planner-app/component/Login.jsx
@@ -0,0 +1,62 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { Colors } from "@/constants/Colors";
+
+export default function Login() {
+  return (
+    <View>
+      <Image
+        source={require("./../assets/images/login.jpg")}
+        style={{ width: "100%", height: "450" }}
+      />
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+            marginTop: 10
+          }}
+        >
+          AI Travel Planner
+        </Text>
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 17,
+            textAlign: "center",
+            color: Colors.GRAY,
+            marginTop: 20
+          }}
+        >
+          Discover your next adventure effortlessly. Personalized itineraries at
+          your fingertips. Travel smarter with AI-driven insights.
+        </Text>
+        <View style={styles.button}>
+          <Text style={{
+            color: Colors.WHITE,
+            textAlign: "center",
+            fontFamily: "outfit-bold" ,
+            fontSize: 17
+          }}>Sign In with Google</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: "100%",
+    padding: 25,
+  },
+  button:{
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 99,
+    marginTop: "25%"
+  }
+});
